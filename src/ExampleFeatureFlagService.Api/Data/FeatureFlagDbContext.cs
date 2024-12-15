2477a6d3638e28e43547679cb999c106a55afd91
Georgi Anastasov <14071890+gvanastasov@users.noreply.github.com>
2024-12-15 23:40:47 +0100

    fix non nullable warn

--- src/ExampleFeatureFlagService.Api/Data/FeatureFlagDbContext.cs
+++ src/ExampleFeatureFlagService.Api/Data/FeatureFlagDbContext.cs
@@ -6,7 +6,10 @@ namespace ExampleFeatureFlagService.Api.Data;
 public class FeatureFlagDbContext : DbContext
 {
     public FeatureFlagDbContext(DbContextOptions<FeatureFlagDbContext> options) 
-        : base(options) { }
+        : base(options) 
+    { 
+        FeatureFlags = Set<FeatureFlag>();
+    }
 
     public DbSet<FeatureFlag> FeatureFlags { get; set; }
 }
